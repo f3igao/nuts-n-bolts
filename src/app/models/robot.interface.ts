a4ea980085bec0d8fa2885bee0e6753a972a731b
f3igao <f3igao@icloud.com>
2018-11-21 15:24:25 -0600

    edit robot interface

---
+++ src/app/models/robot.interface.ts
@@ -0,0 +1,16 @@
+export class IOption {
+  id: number;
+  name: string;
+  value: string;
+  selected?: boolean;
+}
+
+export class IRobot {
+  name: string;
+  dob: string;
+  intel: string;
+  division: string;
+  capabilities: string[];
+  description: string;
+  image?: string;
+}
