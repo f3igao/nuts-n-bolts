0c6b256af4bf78674bb11741e25af159ac806f00
f3igao <f3igao@icloud.com>
2018-11-21 16:04:09 -0600

    add robot list view

--- src/app/feature/army/army.component.ts
+++ src/app/feature/army/army.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { IRobot } from 'src/app/models/robot.interface';
 import * as faker from 'faker';
 
 @Component({
@@ -9,7 +11,7 @@ import * as faker from 'faker';
 })
 export class ArmyComponent implements OnInit {
   accessibleExample: boolean;
-  robotArmy = [];
+  robotArmy: IRobot[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -23,25 +25,31 @@ export class ArmyComponent implements OnInit {
   }
 
   buildRobotArmy(): void {
-    for (let i = 0; i < faker.random.number(50); i++) {
+    const randomCount = Math.floor(Math.random() * 20) + 2;  // a random integer from 2 to 20
+    for (let i = 0; i < randomCount; i++) {
       this.robotArmy.push(this.buildRobot());
     }
   }
 
-  buildRobot(): any {
+  buildRobot(): IRobot {
+    const avatarBaseUrl = 'https://robohash.org/YOUR-TEXT.png';
+    const imageUrl = avatarBaseUrl.replace('YOUR-TEXT', faker.random.word());
+    // const imageUrl = faker.image.avatar();
     return {
       name: faker.name.firstName(),
       dob: faker.date.past().toString().split(' GMT-')[0],
       division: faker.commerce.department(),
       capabilities: this.buildCapabilities(),
       intel: faker.random.arrayElement(['ani', 'agi', 'asi']),
-      image: faker.image.avatar(),
+      image: imageUrl,
+      description: faker.lorem.paragraph(),
     };
   }
 
   buildCapabilities(): string[] {
     const capabilities = [];
-    for (let i = 0; i < faker.random.number(10); i++) {
+    const randomCount = Math.floor(Math.random() * 10) + 1;  // a random integer from 1 to 10
+    for (let i = 0; i < randomCount; i++) {
       capabilities.push(faker.random.word());
     }
     return capabilities;
