197ea81c3384f4aca621df75422e329228e99c42
f3igao <f3igao@icloud.com>
2018-11-23 10:55:18 -0600

    chagne login

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,20 +9,44 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  accessibleExample: boolean;
+  navigationSubscription: any;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+    ) {
+      this.navigationSubscription = this.router.events.subscribe((event: any) => {
+        // subscribe to route change; re-initalise the component on a NavigationEnd event
+        if (event instanceof NavigationEnd) {
+          this.accessibleExample = route.snapshot.params.status === 'good' ? true : false;
+        }
+      });
+    }
 
-  ngOnInit() {
-    this.loginForm = this.fb.group({
-      username: '',
-      password: '',
-    });
+  ngOnInit(): void{
+    this.buildLoginForm();
 
     this.loginForm.valueChanges.subscribe(value => {
       console.log(value);
     });
   }
+
+  buildLoginForm(): void {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: '',
+    });
+  }
+
+  toggleAccessibility(): void {
+    this.accessibleExample = !this.accessibleExample;
+    const status = this.accessibleExample === true ? '/good' : '/bad';
+    this.router.navigate([`/login${status}`]);
+  }
+
   onSubmit() {
-    console.log(this.loginForm.controls);
+    console.log('Submitting:', this.loginForm.controls);
   }
 }
