5ea5f3e3f24dfd50b1162243a6d2a24bd3df8482
f3igao <f3igao@icloud.com>
2018-11-23 10:54:44 -0600

    update todo component

--- src/app/feature/todo/todo.component.ts
+++ src/app/feature/todo/todo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/core/services/http/api.service';
-// import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
-import { ITodo } from 'src/app/models/todo.interface';
+import { ITodo } from 'src/app/models/feature.interface';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -11,22 +11,38 @@ import { ITodo } from 'src/app/models/todo.interface';
 })
 export class TodoComponent implements OnInit {
   todos: ITodo[];
+  navigationSubscription: any;
+  accessibleExample: boolean;
 
   constructor(
     private apiService: ApiService,
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router
+    ) {
+      this.navigationSubscription = this.router.events.subscribe((event: any) => {
+        // subscribe to route change; re-initalise the component on a NavigationEnd event
+        if (event instanceof NavigationEnd) {
+          this.accessibleExample = route.snapshot.params.status === 'good' ? true : false;
+        }
+      });
+    }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.apiService.getToDos().subscribe(data => {
       this.todos = data.slice(0, 15);
     });
   }
 
-  // reorder(event: CdkDragDrop<ITodo>) {
-  //   moveItemInArray(
-  //     this.todos,
-  //     event.previousIndex,
-  //     event.currentIndex
-  //   );
-  // }
+  markDone(i: number, event = null, preventDefault = false): void{
+    if (preventDefault) {
+      event.preventDefault();
+    }
+    this.todos[i].completed = !this.todos[i].completed;
+  }
+
+  toggleAccessibility(): void {
+    this.accessibleExample = !this.accessibleExample;
+    const status = this.accessibleExample === true ? '/good' : '/bad';
+    this.router.navigate([`/todo${status}`]);
+  }
 }
