29149f33b1a2f76a6c8d57fd8321c3c3ebacb0bd
Fei <f3i.g4o@gmail.com>
2019-12-26 18:29:45 -0500

    add about and favicon

--- src/app/shared/about/about.component.ts
+++ src/app/shared/about/about.component.ts
@@ -1,23 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import * as faker from 'faker';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"]
 })
-export class AboutComponent implements OnInit {
-  aboutPara: string[] = [];
-
-  constructor() { }
-
-  ngOnInit() {
-    this.buildPara(5);
-  }
-
-  buildPara(num) {
-    for (let i = 0; i < num; i++) {
-      this.aboutPara.push(faker.lorem.paragraphs());
-    }
-  }
-}
+export class AboutComponent {}
